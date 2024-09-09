37d1bfdb34c224ace8916f3b26713ce3fd889a77
Daniel Rodrigo Engel <d.engel@hotmail.com>
2024-09-09 20:26:06 -0300

    adicionado modulos novos
    Co-authored-by: Angelo Elias Dal Zotto <angelo.dalzotto@edu.pucrs.br>

--- rtl/rodada_simon.sv
+++ rtl/rodada_simon.sv
@@ -1,15 +1,25 @@
 module rodada_simon(
+    input logic clk,
+    input logic rst_n,
     input  logic [127:0] texto,  // Entrada de texto de 128 bits
     input  logic [ 63:0] kj_i,   // Entrada da chave de rodada de 64 bits
     output logic [127:0] cifrado // Saída de texto cifrado de 128 bits
 );
     // Dividir o bloco de 128 bits em dois blocos de 64 bits
-    logic [63:0] PT1, PT2; // Partes 1 e 2 do texto claro de 64 bits cada
-    logic [63:0] CT1, CT2; // Partes 1 e 2 do texto cifrado de 64 bits cada
+    logic [63:0] pt1, pt2; // Partes 1 e 2 do texto claro de 64 bits cada
+    logic [63:0] ct1, ct2; // Partes 1 e 2 do texto cifrado de 64 bits cada
 
-    // Separando os dois blocos de 64 bits a partir dos 128 bits de texto e chave
-    assign PT1 = texto[127:64]; // PT1 representa a primeira metade do texto
-    assign PT2 = texto[63:0];   // PT2 representa a segunda metade do texto
+    always_ff @(posedge clk or negedge rst_n) begin   // Bloco sempre sensível à borda de subida do clock ou borda de descida do reset
+        if (!rst_n) begin                             // Se o reset estiver ativo (baixo)
+            // Separando os dois blocos de 64 bits a partir dos 128 bits de texto e chave
+            pt1 <= texto[63:0];
+            pt2 <= texto[127:64];
+        end
+        else begin                                    // Caso contrário, quando o reset não estiver ativo
+            pt1 <= ct1;
+            pt2 <= ct2;
+        end
+    end
 
     // Implementação da rodada de criptografia Simon
     always_comb begin
@@ -17,18 +27,18 @@ module rodada_simon(
         logic [63:0] rot1, rot8, rot2;
 
         // Realizando as rotações
-        rot1 = {PT1[62:0], PT1[63]};          // Rotação para a esquerda em 1 bit
-        rot8 = {PT1[55:0], PT1[63:56]};       // Rotação para a esquerda em 8 bits
-        rot2 = {PT1[61:0], PT1[63:62]};       // Rotação para a esquerda em 2 bits
+        rot1 = {pt1[62:0], pt1[63]};          // Rotação para a esquerda em 1 bit
+        rot8 = {pt1[55:0], pt1[63:56]};       // Rotação para a esquerda em 8 bits
+        rot2 = {pt1[61:0], pt1[63:62]};       // Rotação para a esquerda em 2 bits
 
         // Cálculo de CT1 usando as operações Simon
-        CT1 = PT2 ^ (rot1 & rot8) ^ rot2 ^ kj_i; 
+        ct1 = pt2 ^ (rot1 & rot8) ^ rot2 ^ kj_i; 
 
         // A segunda parte do texto cifrado (CT2) é apenas a primeira parte do texto claro (PT1)
-        CT2 = PT1;
+        ct2 = pt1;
     end
 
     // Concatenar os resultados para formar a saída de 128 bits do texto cifrado
-    assign cifrado = {CT1, CT2}; // Combinação de CT1 e CT2 para formar o texto cifrado completo de 128 bits
+    assign cifrado = {pt1, pt2}; // Combinação de CT1 e CT2 para formar o texto cifrado completo de 128 bits
 
 endmodule
--- sim_chave.do
+++ sim_chave.do
@@ -1,4 +1,8 @@
+vlib work
+vmap work work
+
 vlog rtl/esquema_chave.sv
 vlog sim/tb_chave.sv
 
 vsim work.tb_chave
+do wave_chave.do
