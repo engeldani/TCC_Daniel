54e55772165f4c76ee534b2872edae980d645390
daniengel <d.engel@hotmail.com>
2024-11-18 20:13:33 -0300

    ajustados os endereços de memória

--- rtl/simon_interface.sv
+++ rtl/simon_interface.sv
@@ -17,18 +17,18 @@ module simon_interface (
     logic [31:0] r_data;
     always_comb begin
         case (addr_i)
-            8'h00:   r_data = pt [127:96];
-            8'h04:   r_data = pt [ 95:64];
-            8'h08:   r_data = pt [ 63:32];
-            8'h0C:   r_data = pt [ 31: 0];
-            8'h10:   r_data = key[127:96];
-            8'h14:   r_data = key[ 95:64];
-            8'h18:   r_data = key[ 63:32];
-            8'h1C:   r_data = key[ 31: 0];
-            8'h20:   r_data = ct [127:96];  // Leitura do ciphertext
-            8'h24:   r_data = ct [ 95:64];
-            8'h28:   r_data = ct [ 63:32];
-            8'h2C:   r_data = ct [ 31: 0];
+            8'h00:   r_data = pt [ 31: 0];
+            8'h04:   r_data = pt [ 63:32];
+            8'h08:   r_data = pt [ 95:64];
+            8'h0C:   r_data = pt [127:96];
+            8'h10:   r_data = key[ 31: 0];
+            8'h14:   r_data = key[ 63:32];
+            8'h18:   r_data = key[ 95:64];
+            8'h1C:   r_data = key[127:96];
+            8'h20:   r_data = ct [ 31: 0];
+            8'h24:   r_data = ct [ 63:32];
+            8'h28:   r_data = ct [ 95:64];
+            8'h2C:   r_data = ct [127:96];  // Leitura do ciphertext
             8'h30:   r_data = {30'b0, valid, start};   // "CSR"
             default: r_data = '0;
         endcase
@@ -56,14 +56,14 @@ module simon_interface (
             if (en_i) begin
                 if (we_i != '0) begin  // Escrita nos registradores
                     case (addr_i)
-                        8'h00:   pt [127:96] <= w_data;
-                        8'h04:   pt [ 95:64] <= w_data;
-                        8'h08:   pt [ 63:32] <= w_data;
-                        8'h0C:   pt [ 31: 0] <= w_data;
-                        8'h10:   key[127:96] <= w_data;
-                        8'h14:   key[ 95:64] <= w_data;
-                        8'h18:   key[ 63:32] <= w_data;
-                        8'h1C:   key[ 31: 0] <= w_data;
+                        8'h00:   pt [ 31: 0] <= w_data;
+                        8'h04:   pt [ 63:32] <= w_data;
+                        8'h08:   pt [ 95:64] <= w_data;
+                        8'h0C:   pt [127:96] <= w_data;
+                        8'h10:   key[ 31: 0] <= w_data;
+                        8'h14:   key[ 63:32] <= w_data;
+                        8'h18:   key[ 95:64] <= w_data;
+                        8'h1C:   key[127:96] <= w_data;
                         8'h30:   start       <= w_data[0];    // "CSR"
                         default: ;
                     endcase
